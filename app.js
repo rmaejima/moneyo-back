eade4e8320dddf35e123e1171ea46c693eea77a5
rmaejima <ryo.maejii@gmail.com>
2022-08-26 16:50:02 +0900

    fix: parse req body

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const testRouter = require('./routes/test/index.js');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   return res.send('Hello');
 });
