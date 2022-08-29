d1835aa408d672c47fb9320aef0fc8cf874eba55
kinouchi takahiro <drums.radwimps203@gmail.com>
2022-08-29 15:08:46 +0900

    [REF] テスト用に作っておいた、いらないコードを削除

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const testRouter = require('./routes/test/index.js');
 const bedinRouter = require('./routes/bedin/index.js');
 const wakeupRouter = require('./routes/wakeup/index.js');
 const sleepRouter = require('./routes/sleep/index.js');
@@ -7,7 +6,6 @@ const idealsleepRouter = require('./routes/idealsleep/index.js');
 
 const cors = require('cors');
 const app = express();
-const port = 8081;
 
 app.use(express.json());
 app.use(cors());
@@ -16,13 +14,8 @@ app.get('/', (req, res) => {
   return res.send('Hello');
 });
 
-app.use('/test', testRouter);
 app.use('/bedin', bedinRouter);
 app.use('/wakeup', wakeupRouter);
 app.use('/sleep', sleepRouter);
 app.use('/idealsleep', idealsleepRouter);
 module.exports = app;
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
