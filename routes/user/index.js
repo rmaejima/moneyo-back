e992aacc5fb75986d7ceba8dc16d41711ecf2d18
kinouchi takahiro <drums.radwimps203@gmail.com>
2022-08-29 14:33:59 +0900

    [ADD] 複数テーブルからの取得できるようにする

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,38 +1,82 @@
 const router = require('express').Router();
+const { default: run } = require('npm-check-updates');
 const AWS = require('../../utils/aws.js');
+const { use } = require('../test/index.js');
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const userTableName = 'MoneyoUser';
 
 router.get('/:userId', (req, res, next) => {
-  // userId からuserの名前を取得
   const userId = req.params.userId;
-  const params = {
-    TableName: userTableName,
-    Key: {
-      userId: userId,
-    },
+  // get user information
+  const user_get_params = {
+    TableName: 'MoneyoUser',
+    Key: { userId },
+  };
+  const user_level_params = {
+    TableName: 'MoneyoUserLevel',
+    Key: { userId },
+  };
+  user_get_sleep_params = {
+    TableName: 'MoneyoIdealSleepTime',
+    Key: { userId },
+  };
+  user_get_castle_params = {
+    TableName: 'MoneyoCastle',
+    Key: { userId },
   };
 
-  dynamo.get(params, function (err, data) {
-    if (err) {
-      console.log(err);
-      const body = { message: '予期せぬエラーが発生しました', err: err };
-      res.status = 500;
-      res.send(body);
-      return;
-    }
-    const item = data.Item;
+  const run = async () => {
+    try {
+      const user = await dynamo.get(user_get_params).promise();
+      const user_level = await dynamo.get(user_level_params).promise();
+      const user_sleep = await dynamo.get(user_get_sleep_params).promise();
+      const user_castle = await dynamo.get(user_get_castle_params).promise();
 
-    if (!item) {
-      const body = { message: 'User データがありません', item: item };
-      res.status = 400;
+      // validation
+      if (
+        !user.Item ||
+        !user_level.Item ||
+        !user_sleep.Item ||
+        !user_castle.Item
+      ) {
+        res.status = 400;
+        const user_lack = user.Item ? '' : 'user ';
+        const user_level_lack = user_level.Item ? '' : 'user_level ';
+        const user_sleep_lack = user_sleep.Item ? '' : 'user_sleep ';
+        const user_castle_lack = user_castle.Item ? '' : 'user_castle ';
+        res.send({
+          message:
+            user_lack +
+            user_level_lack +
+            user_sleep_lack +
+            user_castle_lack +
+            'のデータが存在しません。',
+        });
+        return;
+      }
+
+      // make body
+      const body = {
+        userId: user.Item.userId,
+        name: user.Item.name,
+        level: user_level.Item.level,
+        experiencePoint: user_level.Item.experiencePoint,
+        experiencePointToNextLevel: user_level.Item.experiencePointToNextLevel,
+        bedTime: user_sleep.Item.bedTime,
+        wakeUpTime: user_sleep.Item.wakeUpTime,
+        castlePoint: user_castle.Item.point,
+        castleName: user_castle.Item.name,
+      };
+
+      console.log(body);
+      res.status = 200;
       res.send(body);
-      return;
+    } catch (err) {
+      console.log(err);
+      res.status = 400;
+      res.send({ message: err });
     }
-    res.status = 400;
-    res.send(item);
-    return;
-  });
+  };
+  run();
 });
 
 module.exports = router;
